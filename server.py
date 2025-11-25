0f1a6a75af46754798763c4b7a29b3572a6f5117
luiggiroal <24428203+luiggiroal@users.noreply.github.com>
2025-11-25 06:39:35 -0500

    Code compliance completed

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+"""Executing this function initiates the application of emotions
+detection to be executed over the Flask channel and deployed on
+localhost:5000.
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -6,6 +11,11 @@ app = Flask(__name__)
 
 @app.route("/emotionDetector")
 def sent_detector():
+    """This code receives the text from the HTML interface and
+    runs emotion detection over it using emotion_detector()
+    function. The output returns the score for each
+    emotion and the dominant one for the provided text.
+    """
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
     if response["dominant_emotion"] is None:
@@ -29,6 +39,9 @@ def sent_detector():
 
 @app.route("/")
 def render_index_page():
+    """This function initiates the rendering of the main application
+    page over the Flask channel
+    """
     return render_template("index.html")
 
 
